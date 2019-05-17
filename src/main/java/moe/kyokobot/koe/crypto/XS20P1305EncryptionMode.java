a1ad4b23ebb096cc9c5301df8db37b0db8345006
Alula <6276139+alula@users.noreply.github.com>
2019-05-17 23:00:15 +0200

    Add encryption modes

---
+++ src/main/java/moe/kyokobot/koe/crypto/XS20P1305EncryptionMode.java
@@ -0,0 +1,10 @@
+package moe.kyokobot.koe.crypto;
+
+import io.netty.buffer.ByteBuf;
+
+public class XS20P1305EncryptionMode implements EncryptionMode {
+    @Override
+    public void box(ByteBuf opus, ByteBuf output) {
+
+    }
+}
---
+++ src/main/java/moe/kyokobot/koe/crypto/XS20P1305SuffixEncryptionMode.java
@@ -0,0 +1,10 @@
+package moe.kyokobot.koe.crypto;
+
+import io.netty.buffer.ByteBuf;
+
+public class XS20P1305SuffixEncryptionMode implements EncryptionMode {
+    @Override
+    public void box(ByteBuf opus, ByteBuf output) {
+
+    }
+}
