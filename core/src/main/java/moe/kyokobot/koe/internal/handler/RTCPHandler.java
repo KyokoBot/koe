b0c747e9c637b98f09590c4209f379b3997f71b4
gabixdev <gabixdeveloper@gmail.com>
2019-09-30 21:20:10 +0200

    RTCP stub

---
+++ core/src/main/java/moe/kyokobot/koe/internal/handler/RTCPHandler.java
@@ -0,0 +1,13 @@
+package moe.kyokobot.koe.internal.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+
+public class RTCPHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+    // https://tools.ietf.org/html/rfc3550#section-6
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket packet) throws Exception {
+
+    }
+}
