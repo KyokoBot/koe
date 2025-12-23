134fbf3ede4b58757e2539264fd51960e111f100
Alula <git@alula.me>
2025-12-23 04:48:02 +0100

    Update testbot deps

--- testbot/build.gradle.kts
+++ testbot/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":ext-udpqueue"))
     implementation("net.dv8tion:JDA:5.0.2")
-    implementation("dev.arbjerg:lavaplayer:2.2.3")
-    implementation("com.github.lavalink-devs:lavaplayer-youtube-source:1.13.3")
+    implementation("dev.arbjerg:lavaplayer:2.2.6")
+    implementation("com.github.lavalink-devs:lavaplayer-youtube-source:1.16.0")
     implementation("ch.qos.logback:logback-classic:1.5.18")
 }
