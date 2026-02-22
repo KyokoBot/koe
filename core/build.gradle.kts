45fea56e545546fa27726a9639987dde8469e235
Toπ <git@topi.wtf>
2026-02-22 01:37:42 +0100

    add github actions to publish koe (#42)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,10 +1,21 @@
 dependencies {
-    api("io.netty:netty-transport:4.1.112.Final")
-    implementation("io.netty:netty-codec-http:4.1.112.Final")
-    implementation("io.netty:netty-transport-native-epoll:4.1.112.Final:linux-x86_64")
-    implementation("org.slf4j:slf4j-api:1.8.0-beta4")
-    implementation("com.google.crypto.tink:tink:1.14.1")
-    implementation("moe.kyokobot.libdave:api:1.0-SNAPSHOT")
-    implementation("moe.kyokobot.libdave:impl-jni:1.0-SNAPSHOT")
-    compileOnly("org.jetbrains:annotations:13.0")
+    api(libs.netty.transport)
+    implementation(libs.netty.codec.http)
+    implementation(libs.netty.transport.native.epoll.linux) {
+        artifact {
+            classifier = "linux-x86_64"
+        }
+    }
+
+    implementation(libs.slf4j.api)
+    implementation(libs.tink)
+    implementation(libs.libdave.api)
+    implementation(libs.libdave.impl.jni)
+    implementation(libs.jetbrains.annotations)
+}
+
+mavenPublishing {
+    pom {
+        name = "core"
+    }
 }
