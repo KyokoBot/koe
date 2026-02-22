f3918f0d58f26bf4ead68a883225eee36cc28bc6
topi314 <git@topi.wtf>
2026-02-22 01:46:46 +0100

    remove explicit sources & javadoc jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,6 @@ subprojects {
     configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-
-        withSourcesJar()
-        withJavadocJar()
     }
 
     repositories {
